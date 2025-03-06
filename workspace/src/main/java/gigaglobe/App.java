9c5672884ae12775400b95a6b362b5bafd1d60f9
Seej <xseejcomx@gmail.com>
2025-03-06 17:04:41 +0100

    import javax.swing.*;

--- workspace/src/main/java/gigaglobe/App.java
+++ workspace/src/main/java/gigaglobe/App.java
@@ -1,13 +1,19 @@
 package gigaglobe;
 
-/**
- * Hello world!
- *
- */
+import javax.swing.*;
+
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        int w = 640;
+        int h = 480;
+        JFrame frame = new JFrame();
+
+        frame.setSize(w, h);
+        frame.setTitle("Test");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
