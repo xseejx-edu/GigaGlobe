47abedf05cfe35b6ba933464500ada97a04c5433
xseejx-edu <simone.comin@iiseinaudiscarpa.edu.it>
2025-03-10 11:52:54 +0100

    Added ball

--- workspace/src/main/java/gigaglobe/DrawingCanvas.java
+++ workspace/src/main/java/gigaglobe/DrawingCanvas.java
@@ -2,7 +2,9 @@ package gigaglobe;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Ellipse2D;
 import java.awt.geom.Rectangle2D;
+import java.util.Random;
 
 public class DrawingCanvas extends JComponent{
     private int w;
@@ -15,9 +17,17 @@ public class DrawingCanvas extends JComponent{
 
     @Override
     protected void paintComponent(Graphics g){
+        Random random = new Random();
         Graphics2D g2d = (Graphics2D) g;
+        // Background
         Rectangle2D.Double r  = new Rectangle2D.Double(0,0,w,h);
         g2d.setColor(new Color(100,149,237));
         g2d.fill(r);
+
+        // Ball
+        Humanoid ball = new Humanoid(w/2-25,h/2-25,50,50);
+        Ellipse2D.Double e = new Ellipse2D.Double(ball.w, ball.h, ball.x, ball.y);
+        g2d.setColor(new Color(random.nextInt(256), random.nextInt(256), random.nextInt(256)));//R G B
+        g2d.fill(e);
     }
 }
---
+++ workspace/src/main/java/gigaglobe/Humanoid.java
@@ -0,0 +1,15 @@
+package gigaglobe;
+
+public class Humanoid {
+    public int w;
+    public int h;
+    public int x;
+    public int y;
+    
+    public Humanoid(int w, int h, int x, int y) {
+        this.w = w;
+        this.h = h;
+        this.x = x;
+        this.y = y;
+    }
+}
